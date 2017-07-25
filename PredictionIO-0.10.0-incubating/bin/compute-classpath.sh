58842cfd8532ef62162c6b8e69a9d1d90ea57aa3
Mihaaai <ghidome@gmail.com>
2017-07-25 18:48:54 +0300

    Initialized repository

---
+++ PredictionIO-0.10.0-incubating/bin/compute-classpath.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+SCALA_VERSION=2.10
+
+# Figure out where PredictionIO is installed
+FWDIR="$(cd `dirname $0`/..; pwd)"
+
+. ${FWDIR}/bin/load-pio-env.sh
+
+# Build up classpath
+CLASSPATH="${FWDIR}/conf"
+
+CLASSPATH="$CLASSPATH:${FWDIR}/plugins/*"
+
+ASSEMBLY_DIR="${FWDIR}/assembly"
+
+if [ -n "$JAVA_HOME" ]; then
+  JAR_CMD="$JAVA_HOME/bin/jar"
+else
+  JAR_CMD="jar"
+fi
+
+# Use pio-assembly JAR from either RELEASE or assembly directory
+if [ -f "${FWDIR}/RELEASE" ]; then
+  assembly_folder="${FWDIR}"/lib
+else
+  assembly_folder="${ASSEMBLY_DIR}"
+fi
+
+ASSEMBLY_JAR=$(ls "${assembly_folder}"/pio-assembly*.jar 2>/dev/null)
+
+CLASSPATH="$CLASSPATH:${ASSEMBLY_JAR}"
+
+# Add hadoop conf dir if given -- otherwise FileSystem.*, etc fail ! Note, this
+# assumes that there is either a HADOOP_CONF_DIR or YARN_CONF_DIR which hosts
+# the configurtion files.
+if [ -n "$HADOOP_CONF_DIR" ]; then
+  CLASSPATH="$CLASSPATH:$HADOOP_CONF_DIR"
+fi
+if [ -n "$YARN_CONF_DIR" ]; then
+  CLASSPATH="$CLASSPATH:$YARN_CONF_DIR"
+fi
+if [ -n "$HBASE_CONF_DIR" ]; then
+  CLASSPATH="$CLASSPATH:$HBASE_CONF_DIR"
+fi
+if [ -n "$ES_CONF_DIR" ]; then
+  CLASSPATH="$CLASSPATH:$ES_CONF_DIR"
+fi
+if [ -n "$POSTGRES_JDBC_DRIVER" ]; then
+  CLASSPATH="$CLASSPATH:$POSTGRES_JDBC_DRIVER"
+fi
+if [ -n "$MYSQL_JDBC_DRIVER" ]; then
+  CLASSPATH="$CLASSPATH:$MYSQL_JDBC_DRIVER"
+fi
+
+
+echo "$CLASSPATH"
---
+++ Scripts/get_events.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+pioFolder='~/Projects/pio/PredictionIO-0.10.0-incubating/bin'
+$pioFolder/pio app list
+
