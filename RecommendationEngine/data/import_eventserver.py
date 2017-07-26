869b32986c11c497338ae3dad61fb9baa821fee7
Mihaaai <ghidome@gmail.com>
2017-07-26 17:03:55 +0300

    Started working on importing data

--- RecommendationEngine/data/import_eventserver.py
+++ RecommendationEngine/data/import_eventserver.py
@@ -4,18 +4,16 @@ Import sample data for similar product engine
 
 import predictionio
 import argparse
+import xlrd
 import random
 
-SEED = 3
-
 def import_events(client):
-  random.seed(SEED)
   count = 0
   print client.get_status()
   print "Importing data..."
 
   # generate 10 users, with user ids u1,u2,....,u10
-  user_ids = ["%s" % i for i in ["Lucian", "Elena", "Cristian", "Catalin", "Ciprian", "Florin", "Larisa","Lucian", "Elena", "Cristian", "Catalin", "Ciprian", "Florin", "Larisa"]]
+  user_ids = read_users('UsersPio.xlsx')
   for user_id in user_ids:
     print "Set user", user_id
     client.create_event(
@@ -27,19 +25,25 @@ def import_events(client):
 
   # generate 50 items, with itemEntityId ids i1,i2,....,i50
   # random assign 1 to 4 categories among c1-c6 to items
-  categories = ["c%s" % i for i in ["Lactate", "Electronice", "Bricolaj", "Sucuri", "Ceaiuri"]]
-  item_ids = ["i%s" % i for i in ["Lapte Covalact", "Televizor Samsung FullHD", "Ceai lamaie", "Coca cola", "Grebla"]]
-  for item_id in item_ids:
+  items = read_items('ItemsPio.xlsx')
+  
+  for item_info in items:
+    item_id = item_info[1]
+    category = item_info[0]
     print "Set itemEntityId", item_id
     client.create_event(
       event="$set",
       entity_type="item",
       entity_id=item_id,
       properties={
-        "categories" : random.sample(categories, random.randint(1, 4))
+        "categories" : category
       }
     )
     count += 1
+
+  item_ids = []
+  for item_info in items:
+    item_ids.append(item_info[1])
 
   # each user randomly viewed 5 items
   for user_id in user_ids:
@@ -54,7 +58,7 @@ def import_events(client):
       )
       count += 1
 
-  # each user randomly bought 3 items
+  # each user randomly bought 3  items
   for user_id in user_ids:
     for item in random.sample(item_ids, 3):
       print "User", user_id ,"buys itemEntityId", item
@@ -69,6 +73,22 @@ def import_events(client):
 
 
   print "%s events are imported." % count
+
+def read_users(file):
+  book = xlrd.open_workbook(file)
+  sheet = book.sheet_by_index(0)
+  data = [sheet.cell_value(r, 0).encode('utf-8')  for r in range(sheet.nrows)]
+  # Profit !
+  print data
+  return data
+
+def read_items(file):
+  book = xlrd.open_workbook(file)
+  sheet = book.sheet_by_index(0)
+  data = [[sheet.cell_value(r, c).encode('utf-8') for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+  # Profit !
+  print data
+  return data
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
@@ -85,4 +105,5 @@ if __name__ == '__main__':
     threads=5,
     qsize=500)
   import_events(client)
-
+  #read_users('UsersPio.xlsx')
+  #read_items('ItemsPio.xlsx')
